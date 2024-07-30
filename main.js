de9053ce8254d9cf529d363a894d162e28ef3822
fabio-oliveira83 <fabiooliveira.sjc@hotmail.com>
2024-07-30 10:30:25 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const form = document.getElementById('form-compare');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const campoA = document.getElementById('campo-a');
+    const campoB = document.getElementById('campo-b');
+    
+   
+    let inteiroA = parseInt(campoA.value);
+    let inteiroB = parseInt(campoB.value);
+    
+    let mensagemSucesso = `Muito Bem! O valor B = ${inteiroA} é maior que o valor A = ${inteiroB}! `;
+    let mensagemErro = document.getElementsByClassName('.error-message');
+   
+
+    if(inteiroA == inteiroB){
+       
+        document.querySelector('.error-message').style.display = 'block';
+              
+
+    }
+    else if(inteiroA  > inteiroB){
+
+        document.querySelector('.error-message').style.display = 'block';
+        
+    }else {
+        const inputError = document.querySelector('.mensagem-final');
+        inputError.innerHTML = mensagemSucesso;
+        document.querySelector('.mensagem-final').style.display = 'block';
+        document.querySelector('.error-message').style.display = 'none';
+
+        campoA.value = '';
+        campoB.value = '';
+        
+    }
+                
+})
